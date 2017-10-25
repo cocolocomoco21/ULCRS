4885087677b3b10c1a8a9152b848803fbf646fe2
Matt Henricks <mchenricks@wisc.edu>
2017-10-24 23:29:16 -0500

    Add Gson dependency and add /hello endpoint

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,7 +1,13 @@
+import static spark.Spark.get;
+import com.google.gson.Gson;
+
 public class Server {
 
     public static void main(String args[]) {
         System.out.println("Hello world");
-    }
 
+        Gson gson = new Gson();
+        get("/hello", (request, response) -> "Hello world", gson::toJson);
+    }
+    
 }
