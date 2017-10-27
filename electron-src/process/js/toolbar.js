e16ee0bf2440664bf47d2d464c7d1e4c25ab65bf
xujunjiejack <xujunjiejack@gmail.com>
2017-10-27 12:07:37 -0500

    Created the main interface. Allocated space for ToolBar. Added toolbar.js for Max to work on.

---
+++ electron-src/process/js/toolbar.js
@@ -0,0 +1,20 @@
+let React = require("react");
+
+class Toolbar extends React.Component {
+    render(){
+        return (
+            <div id="toolbar" className="card">
+                <h3 className="card-header">
+                Information
+                </h3>
+
+                <div className = "card-body">
+                    <div className="card-title"> hello </div>
+                    <button type="button" className="btn btn-info"> Button</button>
+                </div> {/*tool bar */}
+            </div>
+            )
+    }
+}
+
+module.exports = Toolbar;
