cf07c3f8c1a0a34c46953590e90c33056a7c31ac
xujunjiejack <xujunjiejack@gmail.com>
2017-10-27 12:07:37 -0500

    Created the main interface. Allocated space for ToolBar. Added toolbar.js for Max to work on.

--- electron-src/process/js/render.js
+++ electron-src/process/js/render.js
@@ -1,3 +1,28 @@
-let bootstrap = require('bootstrap');
-let electron = eRequire('electron');
 
+let ReactDOM = require('react-dom');
+let React = require('react');
+let ToolBar = require('./toolbar');
+
+class MainInterface extends React.Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-4">
+                        <ToolBar />
+                    </div>
+
+                    <div className="col-8">
+
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+ReactDOM.render(
+    <MainInterface />,
+    document.getElementById("ViewTutors")
+);
