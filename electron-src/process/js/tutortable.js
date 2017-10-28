aa4abf487301f735735f404da06b3fa5a1af5649
xujunjiejack <xujunjiejack@gmail.com>
2017-10-28 15:53:17 -0500

    Updated version of both views. Tables have been created for each view.

--- electron-src/process/js/tutortable.js
+++ electron-src/process/js/tutortable.js
@@ -1,10 +1,11 @@
 let ReactDOM = require('react-dom');
 let React = require('react');
 
+
+
 class TutorTable extends React.Component {
     render() {
         return (
-
             <div className="card card-inverse" style={{background: "#ffbf00", border: "#333"}}>
                 <div className="card-block">
                     <h3 className="card-header">
@@ -12,7 +13,30 @@ class TutorTable extends React.Component {
                     </h3>
 
                     <div className = "card-body">
-                        <div className="card-title"> hello </div>
+                        <table className="table table-striped">
+                          <thead>
+                            <tr>
+                              <th>id</th>
+                              <th>First Name</th>
+                              <th>Last Name</th>
+                              <th>Status</th>
+                              <th>Course Preferences</th>
+                              <th>Shift Preferences</th>
+                              <th>Shift Frequency</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <th scope="row">1</th>
+                              <td>{this.props.singleItem.firstName}</td>
+                              <td>{this.props.singleItem.lastName}</td>
+                              <td>{this.props.singleItem.tStatus}</td>
+                              <td>{this.props.singleItem.coursePreference}</td>
+                              <td>{this.props.singleItem.shiftPreference}</td>
+                              <td>{this.props.singleItem.shiftFrequency}</td>
+                            </tr>
+                          </tbody>
+                        </table>
                     </div> {/*tool bar */}
                 </div>
             </div>
--- electron-src/process/js/viewschedules.js
+++ electron-src/process/js/viewschedules.js
@@ -3,18 +3,30 @@ let React = require('react');
 
 let ScheduleToolBar = require('./scheduletoolbar');
 let ScheduleTable = require('./scheduletable');
-console.log(ScheduleToolBar.toString());
+let fs = eRequire('fs');
+let loadSchedules = JSON.parse(fs.readFileSync(dataLocation));
+console.log(loadSchedules);
 class ViewSchedulePage  extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            schedules : loadSchedules
+        }
+    }
     render() {
         return (
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-4">
+                    <div className="col-2">
                         {/* <ScheduleToolbar /> */}
                     </div>
 
                     <div className="col-8">
-                        <ScheduleTable />
+                        <ScheduleTable schedules={this.state.schedules}/>
+                    </div>
+
+                    <div className="col-2">
+                        
                     </div>
                 </div>
             </div>
