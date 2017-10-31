78a3d47e41da18f0cf81256fa0b6bd3f55a2fb03
getsong <getsong13@gmail.com>
2017-10-30 23:18:53 -0500

    dynamic scheduleTable with scheduleData.json format changed

--- electron-src/process/js/scheduletable.js
+++ electron-src/process/js/scheduletable.js
@@ -23,9 +23,38 @@ class ShiftCard extends React.Component {
             </td>
         )
     }
-
 }
 
+class Row extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            schedules: this.props.schedules,
+            j: this.props.j
+        }
+    }
+    render(){
+        var emptyRow = true;
+        let row = [];
+        for (var i = 0; i < this.state.schedules.length; i++){
+            console.log(i);
+            if (this.state.schedules[i].Data.length >= this.state.j){
+                emptyRow = false;
+                row.push(<td><ShiftCard info={this.state.schedules[i].Data[this.state.j]} /></td>);
+            }
+            else{
+                row.push(<td></td>);
+            }
+        }
+        console.log(row);
+        if (emptyRow==true){
+            return([]);
+        }
+        else{
+            return(row);
+        }
+    }
+}
  
 
 class ScheduleTable extends React.Component {
@@ -37,44 +66,65 @@ class ScheduleTable extends React.Component {
     }
     // Put your code here.
     render() {
-        let cards = [<ShiftCard info={this.state.schedules[0].Sunday1[0]} key="0"/>,   
-        <ShiftCard info={this.state.schedules[0].Sunday1[1]} key="1" />,
-        <ShiftCard info={this.state.schedules[0].Sunday1[2]} key="2" />]
-        return (
+        let a = [];
+        for (var i = 0; i < this.state.schedules.length; i++){
+            a.push(<th>{this.state.schedules[i].Slot}</th>)
+        };
+        let cards = [<ShiftCard info={this.state.schedules[0].Data[0]} key="0"/>,
+        <ShiftCard info={this.state.schedules[1].Data[0]} key="1" />,
+        <ShiftCard info={this.state.schedules[2].Data[0]} key="2" />];
+        console.log(this.state.schedules);
 
-            <div className="card" style={{background: "#00ffff", border: "#333"}}>
+        let b = [];
+        var j = 0;
+        while (true){
+            console.log(j);
+            // let row = <Row schedules={this.state.schedules} j={j} />;
+            // console.log(row == []);
+            var emptyRow = true;
+            let row = [];
+            for (var i = 0; i < this.state.schedules.length; i++){
+                console.log(i);
+                if (this.state.schedules[i].Data.length > j){
+                    emptyRow = false;
+                    row.push(<td><ShiftCard info={this.state.schedules[i].Data[j]} /></td>);
+                }
+                else{
+                    row.push(<td></td>);
+                }
+            }
+            console.log(emptyRow);
+            if (emptyRow==true){
+                break;
+            }
+            else{
+                b.push(<tr>{row}</tr>);
+            }
+            j++;
+            // b.push(<tr>{row}</tr>);
+            // if (j==3){
+            //     break;
+            // }
+        }
+        let row = <Row schedules={this.state.schedules} j={3} />;
+        console.log("test");
+        console.log(row == []);
+        console.log(row);
+        return (
+            <div className="card" style={{overflow: "auto", background: "#00ffff", border: "#333"}}>
                 <div className="card-block">
                     <h3 className="card-header">
                         Schedule: schedule 1
                     </h3>
-
                     <div className = "card-body">
                     <table className="table">
                         <thead>
                             <tr>
-                                <th>Sunday</th>
-                                <th>Monday</th>
-                                <th>Tuesday</th>
-                                <th>Wednesday</th>
+                                {a}
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                {cards}
-                                <td>Chandler</td>
-                            </tr>
-                            <tr>
-                                <td scope="row"></td>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                            <tr>
-                                <td scope="row">3</td>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
+                            {b}
                         </tbody>
                     </table>
                     </div> {/*tool bar */}
