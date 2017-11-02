6620e8219b8bfcbf83a8b4f6b7f2871dda6ae27b
Jason Cheng <chhk1211@gmail.com>
2017-11-01 20:41:44 -0500

    Add controller unit test frameworks. Implement test cases with fake data.

---
+++ src/test/java/ulcrs/controllers/SessionControllerTest.java
@@ -0,0 +1,67 @@
+package ulcrs.controllers;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.powermock.reflect.Whitebox;
+import spark.Request;
+import spark.Response;
+import ulcrs.models.session.Session;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
+
+public class SessionControllerTest {
+
+    private SessionController sessionControllerTest;
+
+    @Mock
+    private Request requestMock;
+
+    @Mock
+    private Response responseMock;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+
+        sessionControllerTest = new SessionController();
+    }
+
+    @Test
+    public void successGetSessionList() throws Exception {
+        List<Session> getSessionListResult = Whitebox.invokeMethod(sessionControllerTest, "getSessionList",
+                requestMock, responseMock);
+        // TODO: implement test case
+        Assert.assertEquals(null, getSessionListResult);
+    }
+
+    @Test
+    public void successGetSession() throws Exception {
+        Session getSessionResult = Whitebox.invokeMethod(sessionControllerTest, "getSession",
+                requestMock, responseMock);
+        // TODO: implement test case
+        Assert.assertEquals(null, getSessionResult);
+    }
+
+    @Test
+    public void successUpdateSession() throws Exception {
+        Boolean updateSessionResult = Whitebox.invokeMethod(sessionControllerTest, "updateSession",
+                requestMock, responseMock);
+        // TODO: implement test case
+        Assert.assertEquals(false, updateSessionResult);
+    }
+
+    @Test
+    public void successDeleteSession() throws Exception {
+        Boolean deleteSessionResult = Whitebox.invokeMethod(sessionControllerTest, "deleteSession",
+                requestMock, responseMock);
+        // TODO: implement test case
+        Assert.assertEquals(false, deleteSessionResult);
+    }
+}
