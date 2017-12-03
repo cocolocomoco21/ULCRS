cf39da2f3e251c9000c310c9ce577148d5df70e7
William Jen <wjen@wisc.edu>
2017-12-03 11:47:26 -0600

    Initial commit for requirement ui.

---
+++ CourseRequirementGenerator/CourseRequirementGenerator/Course.cs
@@ -0,0 +1,9 @@
+﻿
+namespace CourseRequirementGenerator
+{
+    public class Course
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ CourseRequirementGenerator/CourseRequirementGenerator/CourseRequirement.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace CourseRequirementGenerator
+{
+    public class CourseRequirement
+    {
+        public int courseID { get { return course.id; } }
+
+        [JsonIgnore]
+        public string courseName { get { return course.name; } }
+
+        [JsonIgnore]
+        public Course course { get; set; }
+
+        public string intensity { get; set; }
+        public int timesPerWeek { get; set; }
+        public int numTutors { get; set; }
+        public List<CourseRequirementSpecifics> specifics { get; set; }
+    }
+    
+    public class CourseRequirementSpecifics
+    {
+        [JsonIgnore]
+        public Course course { get; set; }
+
+        [JsonIgnore]
+        public Shift shift { get; set; }
+
+        public int shiftID { get { return shift.id; } }
+        public int numTutors { get; set; }
+    }
+}
