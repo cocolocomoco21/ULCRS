d99e6cba8b1ab1184e4419ad175ed93e371e9b89
DooSol <doosol@DooSols-MacBook-Pro.local>
2017-12-06 13:38:06 -0600

    bootstrap struct change

--- electron-src/process/js/scheduletable.js
+++ electron-src/process/js/scheduletable.js
@@ -39,7 +39,7 @@ class ScheduleTable extends React.Component {
     scheduleName() {
         return "Schedule " + (this.state.index+1);
     }
-  
+
     render() {
 
         let colors = [["#428BCA", "#5CC3E1"], ["#468847", "#46A546"],
@@ -97,17 +97,13 @@ class ScheduleTable extends React.Component {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col">
-                        <div className="row" style={{background: "#c5050c", color: "#f9f9f9"}}>
-                            <div className="col-9">
+                    <div className="card">
+                        <div className="card-header" style={{background: "#c5050c", color: "#f9f9f9"}}>
                                 <h3> {this.scheduleName()} </h3>
-                            </div>
-                            <div className="col-3">
                                 <h5> Rating: {this.state.schedules[this.state.index].rating} </h5>
-                            </div>
                         </div>
                         <div className="row container-scroll">
-                            <div className="col" style={{padding: 0}}>
+                            <div className="col">
                                 <div className="row" style={{margin: 0, width: "100%", height: "500px"}}>
                                     {containerList}
                                 </div>
